1c61df8422c8f49be069895b6a6331eade2341d1
jayware9 <jay.ware@gov.scot>
2024-03-26 16:04:25 +0000

    remove old session 3 files

--- session3/save_data.R
+++
@@ -1,7 +0,0 @@
-load("./Data/RAP_toy_data.Rda")
-
-write.csv(workings_days, "./Data/working_days.csv")
-write.csv(Benefit1, "./Data/Benefit1.csv")
-write.csv(Benefit2, "./Data/Benefit2.csv")
-write.csv(Benefit3, "./Data/Benefit3.csv")
-write.csv(Benefit4, "./Data/Benefit4.csv")
